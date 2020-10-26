e96ae5c58b381d706359a8a5c3d74824697e0ef4
Yakymenko110 <maksmaksim20022412@gmail.com>
2020-10-26 16:36:38 +0200

    add "Exercise.cpp"

---
+++ PR04.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<math.h>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int m, n, s;
+	double V, X, Z, C;
+	cout << "Введіть ціну" << endl;
+	cout << "Грн.=" << endl;
+	cin >> m;
+	cout << "Коп.=" << endl;
+	cin >> n;
+	cout << "Введіть кошти, грн.=";
+	cin >> s;
+	V = double (n) / 100;
+	Z = m + V;
+	X = s / Z;
+	cout << "кількість зошитів = " << X << endl;
+}
+
