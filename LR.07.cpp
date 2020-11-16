6c4062972d2f08fe1aaae8f0a0e72ba446b8182c
Maxim Yakymenko <maksmaksim20022412@gmail.com>
2020-11-16 20:13:53 +0200

    add StudBr

---
+++ LR.07.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int x, n = 3;
+	cout << "Enter x =";
+	cin >> x;
+	float y, z;
+	const double pi = 3.14;
+	int sum = 0;
+	for (int i = 1; i < 4; i++)
+	{
+		sum = sum + pow(x, i);
+	}
+    y = log(pow(x,3)) + cos(sum * pi / 180);
+	int prod = 0;
+	for (int i = 1; i < 10; i++) 
+	{
+		prod = prod * log(i * y);
+	}
+    z = cos(prod * pi / 180) / (y + 2);
+	cout << "Y = " << y << endl;
+	cout << "Z = " << z << endl;
+}
