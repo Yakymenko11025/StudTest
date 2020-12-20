eb1eb6272dfabe061ef6c61af65950ed3307dbd2
Maxim Yakymenko <maksmaksim20022412@gmail.com>
2020-12-20 19:10:50 +0200

    add LR.09

---
+++ LR.09.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main()
+{
+    const int n = 5;
+    int A[n];
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) 
+    {
+        A[i] = rand() % (101) - 50;
+        cout << setw(5) << A[i];
+    }
+    
+    int max = 0;
+    for (int i = 0; i < n; i++)
+        if (A[i] > A[max]) max = i;
+
+    cout << endl << setw(5) << A[max] << setw(5) << max;
+}
--- Source.cpp
+++ Source.cpp
@@ -21,6 +21,4 @@ int main()
 		cout << setw(10) << "x=" << x << setw(10) << "y=" << y << endl;
 		x += h;
 	}
-	cout << "Goodbye" << endl;
-	cout << "Thank you" << endl;
 }
